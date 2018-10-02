317fb36fa2af75b74ac29a105b8286a492d2c5b1
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-10-01 21:49:33 -0400

    Finished second activity that gets triggered after 5seconds from the SpashScreen. The logic of playing music is what i have to do next.

--- app/src/main/java/com/adefemikolawole/alohamusic/SplashActivity.java
+++ app/src/main/java/com/adefemikolawole/alohamusic/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.adefemikolawole.alohamusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        };
+        Timer opening = new Timer(); // an instance of timer
+        opening.schedule(task, 5000); // timer scheduled for 5 seconds.
+        finish(); //releases the resources that were created for the Splash Activity and closes the activity.
+        startActivity(new Intent(SplashActivity.this, MainActivity.class)); //start activity in SplachAcvtivity launches the second class whichh is MainActivity
+
+
+
     }
 }
