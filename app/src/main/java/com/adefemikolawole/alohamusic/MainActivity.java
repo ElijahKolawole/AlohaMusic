3524c447b7a355631abbd7cdf200be33f23a23f1
Adefemi Kolawole <adefemikolawole@gmail.com>
2018-10-02 11:59:21 -0400

    changed literal texts into String resources and them imported the R package to avoid other issues.

--- app/src/main/java/com/adefemikolawole/alohamusic/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/alohamusic/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 
 import java.time.LocalDate;
 
+import static com.adefemikolawole.alohamusic.R.*;
+
 public class MainActivity extends AppCompatActivity {
     Button button1, button2;
     MediaPlayer mpUkulele, mpDrums;
@@ -20,17 +22,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
         Log.d(TAG, "Starting Application from MainActivity");
 
-        button1 = findViewById(R.id.btnUkulele);
-        button2 = findViewById(R.id.btnDrums);
+        button1 = findViewById(id.btnUkulele);
+        button2 = findViewById(id.btnDrums);
         button1.setOnClickListener(btUkulele);
         button2.setOnClickListener(btDrums);
         mpUkulele = new MediaPlayer();
-        mpUkulele = MediaPlayer.create(this, R.raw.ukulele);
+        mpUkulele = MediaPlayer.create(this, raw.ukulele);
         mpDrums = new MediaPlayer();
-        mpDrums = MediaPlayer.create(this, R.raw.drums);
+        mpDrums = MediaPlayer.create(this, raw.drums);
         playing = 0;
 
     }
@@ -43,13 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 case 0:
                     mpUkulele.start();
                     playing = 1;
-                    button1.setText("Pause Ukulele Song");
+                    button1.setText(string.txtPauseUku);
                     button2.setVisibility(View.INVISIBLE);
                     break;
                 case 1:
                     mpUkulele.pause();
                     playing = 0;
-                    button1.setText("Play Ukulele Song");
+                    button1.setText(string.txtPauseUku);
                     button2.setVisibility(View.VISIBLE);
                     break;
             }
@@ -57,21 +59,19 @@ public class MainActivity extends AppCompatActivity {
     };
 
     Button.OnClickListener btDrums = new Button.OnClickListener(){
-
-
         @Override
         public void onClick(View v) {
             switch(playing){
                 case 0:
                     mpDrums.start();
                     playing = 1;
-                    button2.setText("Pause Drum Song");
+                    button2.setText(string.txtPlayDru);
                     button1.setVisibility(View.INVISIBLE);
                     break;
                 case 1:
                     mpDrums.pause();
                     playing = 0;
-                    button2.setText("Play Drum Song");
+                    button2.setText(string.txtPauseDru);
                     button1.setVisibility(View.VISIBLE);
                     break;
             }
